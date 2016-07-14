19e165ed3cc77172be3a4d554aef9c8eced6b8b1
blank-blank <timlewis4645@gmail.com>
2016-07-14 14:06:53 -0500

    make default params. crate mvp template

---
+++ cloudformation/launch_cf_stack.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$TEMPLATE_DIR" ]
+then
+    TEMPLATE_DIR="$(dirname $0)/templates"
+fi
+TEMPLATE_FILE="$1"
+TEMPLATE_FILEPATH="${TEMPLATE_DIR}/${TEMPLATE_FILE}"
+
+if [ -z $TEMPLATE_FILE ]
+then
+    echo "Usage: launch_cf_template template_file"
+    exit 1
+fi
+
+if [ ! -f $TEMPLATE_FILEPATH ]
+then
+    echo $TEMPLATE_FILEPATH
+    echo "TEMPLATE_FILEPATH does not exist" 
+    exit 1
+fi
+stack_name="mini_project_stack"
+region="us-west-2"
+keypair_name="tim_keypair"
+instance_size="t2.small"
+template_name="mini_project_template.json"
+
+aws cloudformation create-stack --stack-name "$stack_name" --region "$region" --parameters ParameterKey=KeyName,ParameterValue="$keypair_name" ParameterKey=InstanceType,ParameterValue="instance_size" --template-body file:////$TEMPLATE_DIR//$template_name
