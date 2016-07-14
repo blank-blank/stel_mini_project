9058e91e4a3f132db0350bbdbebf782999eb14df
blank-blank <timlewis4645@gmail.com>
2016-07-14 12:45:36 -0500

    add red tests

---
+++ tests/run_tests.py
@@ -0,0 +1,22 @@
+#================================================================
+# tests for stelligent mini-project
+#================================================================
+
+import urllib2
+
+
+def test_server_is_up(url):
+	'''
+	test whether the server is up and returning a 200
+	'''
+
+	assert false
+	#urllib2.urlopen(
+
+
+def test_correct_message(test_url):
+	'''
+	test whether the text displayed on the website is correct
+	'''
+
+	assert false
