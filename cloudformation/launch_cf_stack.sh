e8415f16366609976662356f5aa761cb98781889
EC2 Default User <ec2-user@ip-172-31-18-54.us-west-2.compute.internal>
2016-07-18 22:21:54 +0000

    delete stack if already exists

--- cloudformation/launch_cf_stack.sh
+++ cloudformation/launch_cf_stack.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 if [ -z "$TEMPLATE_DIR" ]
 then
@@ -20,12 +19,65 @@ then
     echo "TEMPLATE_FILEPATH does not exist" 
     exit 1
 fi
-stack_name="miniProjectStack"
+#stack_name="miniProjectStack"
+stack_name="instancestack"
+
 region="us-west-2"
 keypair_name="tim_keypair"
 instance_size="t2.small"
 template_name="mini_project_template.json"
 
+
+create_stack() { 
+
+    aws cloudformation create-stack --stack-name "$1" --region "$region" --parameters ParameterKey=KeyName,ParameterValue="$keypair_name" ParameterKey=InstanceType,ParameterValue="$instance_size" --template-body file://$TEMPLATE_DIR/$template_name
+
+}
+
+delete_stack() {
+
+    aws cloudformation delete-stack --stack-name "$1"
+    while stack_exists $1
+    do 
+        echo "sleeping"
+        sleep 2 
+   done
+}
+
+stack_exists() {
+    if  aws cloudformation describe-stacks --stack-name $1 &> //dev/null
+    then
+        return 0
+    else
+        return 1
+    fi 
+}
+
+if stack_exists $stack_name
+then
+
+    delete_stack $stack_name
+    create_stack $stack_name
+
+    #if [ "$clobber" = "yes" ] 
+    #then
+    #    echo "deleting stack with name: $stack_name "
+    #    delete_stack $stack_name
+
+    #else
+    #    echo "stack with name: $stack_name already exists, but clobber is set to no. Nothing will be changed." 
+    #    exit
+    #fi 
+else
+    echo 'nope'
+fi
+exit
+
 aws cloudformation validate-template --template-body file://$TEMPLATE_FILEPATH
 
-aws cloudformation create-stack --stack-name "$stack_name" --region "$region" --parameters ParameterKey=KeyName,ParameterValue="$keypair_name" ParameterKey=InstanceType,ParameterValue="$instance_size" --template-body file://$TEMPLATE_DIR/$template_name
+aws cloudformation describe-stacks 
+
+#aws cloudformation update-stack --stack-name "$stack_name" --region "$region" --parameters ParameterKey=KeyName,ParameterValue="$keypair_name" ParameterKey=InstanceType,ParameterValue="$instance_size" --template-body file://$TEMPLATE_DIR/$template_name
+
+
+
